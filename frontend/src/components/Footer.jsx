3125a1be082bbe529f65b486475c6788aa749875
Monishalakshmipathy <monishalakshmipathy@gmail.com>
2025-04-13 15:42:46 +0530

    initisl commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(232, 227, 227)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    <h5>
+                        <span style={{color: "blue"}}>Developed by </span>
+                        <span style={{color: "blue"}}>Monisha lakshmipathy</span>
+                    </h5>
+            </div> 
+
+            <div className="text-center pt-1">
+                <a href="https://www.linkedin.com/in/monisha-lakshmipathy-a43a79213" target="_blank" rel="noreferrer">
+                    <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+
+                <a href="https://github.com/Monishalakshmipathy" target="_blank" rel="noreferrer">
+                    <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
+                <a href="mailto:monishalakshmipathy@gmail.com" target="_blank" rel="noreferrer">
+                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+            </div>
+            {/* <footer style={{backgroundColor: 'crimson', color:'springgreen', textAlign: 'center', position:'fixed', width: '100%',height : '50px', bottom : '0', left: '0', right: '0', zIndex : '999'}}><h1>THIS IS MY FOOTER</h1></footer> */}
+        </div>
+       
+    )
+}
